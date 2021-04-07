6fc1b633447c43fcb9a4b2b5c8d72eaeced6b956
Nuria <soriano.nuria@gmail.com>
2021-04-07 09:44:42 +0200

    fix dev server issue

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,7 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
+  target: "web",
   devServer: {
     contentBase: "./dist",
   },
